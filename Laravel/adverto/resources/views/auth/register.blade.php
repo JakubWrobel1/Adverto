021f0b00f4ee153870d39e63a1d8ffe89181f8e5
JakubWrobel1 <kubawbl@gmail.com>
2023-05-27 16:53:32 +0200

    register responsive

--- Laravel/adverto/resources/views/auth/register.blade.php
+++ Laravel/adverto/resources/views/auth/register.blade.php
@@ -6,52 +6,33 @@
     
     <title>Rejestracja - Adverto</title>
 
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    
-    <!-- My CSS stylesheet -->
-    <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
-
-    <!-- Font Awesome -->
+    @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- Quicksand Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
-
-    <!-- jquery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     
-    <!-- Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    
-    <!-- JavaScript Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
     
 </head>
-<body>
-    <div class="signup-wrap">
-        <div class="signup-top">
-        <div class="signup-logo">
+<body class="bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-center ">
+    <div class="bg-white w-screen md:max-w-md md:h-4/5 h-screen md:rounded-lg  flex flex-col shadow-2xl">
+        <div class="justify-center p-3 flex-grow">
+        <div class="mb-5 p-2">
                 <a href="{{url('welcome')}}">
                     <img src="{{asset('img/images/icons/logo_blue.png')}}" alt="Adverto">
                 </a>
             </div>
             <form method="POST" action="{{route('register')}}">
                @csrf
-               <div class="input-wrap">
-                    <input type="text" name="name" placeholder="Imię i nazwisko*" value="{{ old('name') }}" required />
+               <div class="flex flex-col mb-4">
+                    <input  class="w-full rounded-lg border-none  " type="text" name="name" placeholder="Imię i nazwisko*" value="{{ old('name') }}" required />
                     @error('name')
-                        <div class="alert alert-danger">
+                        <div class="text-red-500">
                             {{$message }}
                         </div>
                     @enderror
                 </div>
-               <div class="input-wrap">
-                    <input type="text" name="username" placeholder="Nazwa użytkownika" value="{{ old('username') }}" required />
+               <div class="flex flex-col mb-4">
+                    <input class="w-full rounded-lg border-none  " type="text" name="username" placeholder="Nazwa użytkownika" value="{{ old('username') }}" required />
                     @error('username')                       
-                        <div class="alert alert-danger">
+                        <div class="text-red-500">
                             @if($message ==='Pole username już istnieje.')
                                 Nazwa użytkownika jest zajęta
                              @else
@@ -59,16 +40,16 @@
                             @endif</div>
                     @enderror
                 </div>
-                <div class="input-wrap">
-                    <input type="email" name="email" placeholder="Email*"  value="{{ old('email') }}" required />
+                <div class="flex flex-col mb-4">
+                    <input class="w-full rounded-lg border-none  " type="email" name="email" placeholder="Email*"  value="{{ old('email') }}" required />
                     @error('email')
-                        <div class="alert alert-danger">{{$message }}</div>
+                        <div class="text-red-500">{{$message }}</div>
                     @enderror
                 </div>
-                <div class="input-wrap">
-                <input type="password" name="password" placeholder="Hasło*" required/>
+                <div class="flex flex-col mb-4">
+                <input class="w-full rounded-lg border-none  " type="password" name="password" placeholder="Hasło*" required/>
                     @error('password')
-                        <div class="alert alert-danger">
+                        <div class="text-red-500">
                             @if ($message === 'Pole password musi mieć co najmniej 8 znaków.')
                                 Hasło musi mieć co najmniej 8 znaków.
                             @elseif($message === 'Potwierdzenie pola password nie zgadza się.')
@@ -80,36 +61,36 @@
                         </div>
                     @enderror
                 </div>
-                <div class="input-wrap">
-                    <input type="password" name="password_confirmation" placeholder="Wpisz ponownie hasło*" />                  
+                <div class="flex flex-col mb-4">
+                    <input class="w-full rounded-lg border-none  " type="password" name="password_confirmation" placeholder="Wpisz ponownie hasło*" />                  
                 </div>
-                <div class="input-wrap-left">
+                <div class="flex justify-center items-center mb-4">
                     
-                    <p>Akceptuję regulamin sklepu internetowego*</p>
+                    <p>Akceptuję regulamin sklepu internetowego &ensp;</p>
                     <input type="checkbox" name="terms" required/>
                     @error('terms')
-                        <div class="alert alert-danger">{{$message }}</div>
+                        <div class="text-red-500">{{$message }}</div>
                     @enderror
                 </div>
-                <div class="input-wrap">
-                    <button type="submit">Zarejestruj się</button>
-                                
-
+                <div class="flex justify-center h-12">
+                    <button type="submit" class="w-screen md:w-1/2 hover:bg-cyan-400 bg-blue-500 md:rounded-full  text-xl text-white transition duration-700 transform hover:scale-95">Zarejestruj się</button>
                 </div>
             </form>
-            <div class="input-wrap">
-                <a href="{{url('login')}}"><button>Zaloguj się</button></a>
+            <div class="flex justify-center">
+                <a href="{{url('login')}}" class="w-screen md:w-1/2  bg-blue-500 md:rounded-full text-xl text-white flex justify-center h-12 mb-10 text-white  hover:bg-cyan-400 transition duration-700 transform hover:scale-95"><button>Zaloguj się</button></a>
             </div>
         </div>
 
                     
-        <div class="signup-bottom">
-            <h3>Lub zaloguj się za pomocą: </h3>
-            <div class="link-container">
-                <ul>
-                    <li><i class="fab fa-facebook"></i></li>
-                    <a href="/auth/google/redirect"><li><i class="fab fa-google-plus-g"></i></li></a>
-                </ul>
+        <div class="  flex flex-col items-center text-lg bg-gradient-to-r from-cyan-500 to-blue-500">
+            <h3 class="text-white">Lub zaloguj się za pomocą: </h3>
+            <div class="flex justify-center mb-10 text-2xl">
+                <div class="p-2 hover:text-white">
+                    <a href="/auth/facebook/redirect"><sapn class="fab fa-facebook"></a>
+                </div>
+                <div class="p-2 hover:text-white ">
+                    <a href="/auth/google/redirect"><span class="fab fa-google-plus-g"></a>
+                </div>
             </div>
         </div>
     </div>
