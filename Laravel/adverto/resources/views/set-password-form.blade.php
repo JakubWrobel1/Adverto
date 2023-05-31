239e6d3e1caae98a1cf8cf3428f4ba710c8ff59a
JakubWrobel1 <96049318+JakubWrobel1@users.noreply.github.com>
2023-05-31 15:28:23 +0200

    Css (#2)
    * przykładowy wygląd ogłoszeń na stronie głównej
    
    * database

--- Laravel/adverto/resources/views/set-password-form.blade.php
+++ Laravel/adverto/resources/views/set-password-form.blade.php
@@ -3,47 +3,22 @@
     
     <!-- Mobile responsibility -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+    @vite('resources/css/app.css')
     <title>Logowanie - Adverto</title>
 
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    
-    <!-- My CSS stylesheet -->
-    <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- Quicksand Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
-
-    <!-- jquery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    
-    <!-- Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    
-    <!-- JavaScript Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
-
-    <!-- My JavaScript file-->
-    <script src="./assets/js/commonActions.js"></script>
 </head>
-<body>
-    <div class="signup-wrap">
-        <div class="signup-top">
-            <div class="signup-logo">
+<body class="bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-center">
+    <div class="bg-white w-screen md:max-w-md md:h-2/5 h-screen md:rounded-lg  flex flex-col shadow-2xl">
+        <div class="justify-center p-3 flex-grow">
+            <div class="mb-5 p-2">
                 <a href="{{url('welcome')}}">
-                    <img src="{{asset('img/images/icons/logo_blue.png')}}" alt="Adverto">
+                    <img class="scale-75" src="{{asset('img/images/icons/logo_blue.png')}}" alt="Adverto">
                 </a>
             </div>
             <form method="POST" action="{{ route('set-password') }}" id="set-password">
                 @csrf               
-                <div class="input-wrap">
-                    <input type="password" name="password" placeholder="Ustaw hasło dla swojego konta" required/>
+                <div class="flex flex-col mb-4">
+                    <input class="w-full rounded-lg border-none" type="password" name="password" placeholder="Ustaw hasło dla swojego konta" required/>
                     @error('password')                      
                         <div class="alert alert-danger">
                             @if($message === 'Potwierdzenie pola password nie zgadza się.')
@@ -54,17 +29,17 @@
                         </div>
                     @enderror
                 </div>
-                <div class="input-wrap">
-                    <input type="password" name="password_confirmation" placeholder="Wpisz ponownie hasło" required/>
+                <div class="flex flex-col mb-4">
+                    <input class="w-full rounded-lg border-none" type="password" name="password_confirmation" placeholder="Wpisz ponownie hasło" required/>
                     @error('password_confirmation')
                         <div class="alert alert-danger">{{$message }}</div>
                     @enderror
                 </div>
                 
-                <div class="input-wrap">
-                    <button type="submit">{{__('Zapisz')}}</button>
+                <div class="flex justify-center h-12">
+                    <button class="w-screen md:w-1/2 hover:bg-cyan-400 bg-blue-500 md:rounded-full  text-xl text-white transition duration-700 transform hover:scale-95 " type="submit">{{__('Zapisz')}}</button>
                 </div>
-            <p>Jeśli nie ustawisz teraz hasła będziesz mógł to zrobić później w ustawieniach konta -> <a href="{{url('my-account')}}">Moje konto<a></p>
+            <p class="pt-5">Jeśli nie ustawisz teraz hasła będziesz mógł to zrobić później w ustawieniach konta</p>
             </form>            
         </div>
     </div>
--- Laravel/adverto/resources/views/welcome.blade.php
+++ Laravel/adverto/resources/views/welcome.blade.php
@@ -7,102 +7,126 @@
 <head>
     <meta charset="utf-8">
     @vite('resources/css/app.css')
+    
     <!-- Mobile responsibility -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Adverto - Serwis ogłoszeniowy</title>  
 </head>
-<body class="bg-gradient-to-r from-cyan-500 to-blue-500">
+<body class="bg-gradient-to-r from-cyan-500 to-blue-500 overflow-x-hidden">
     <div class="flex">
-            <div class="w-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-between pt-5 pb-5 ">
-            <div class="w-screen scale-75  md:scale-100 md:w-60 md:pl-10">
-                    <a href="{{url('welcome')}}">
-                        <img class="h-full w-full" src="{{asset('img/images/icons/logo.png')}}" alt="Adverto">
-                    </a>
-                </div>
-                <div class="flex group inline-block relative">                     
-                    <button class="flex pt-2">
-                        @if (Route::has('login'))
+        <div class=" w-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-between pt-5 pb-5">
+            <div class="w-screen scale-75 md:scale-100 md:w-60 md:pl-10">
+                <a href="{{ url('welcome') }}">
+                    <img class="h-full w-full" src="{{ asset('img/images/icons/logo.png') }}" alt="Adverto">
+                </a>
+            </div>
+            <div class="flex group inline-block relative">
+                <button class="flex pt-2">
+                    @if (Route::has('login'))
                         @auth
                             <div class="w-auto bg-inherit text-white">
                                 <div class="hover:text-slate-400">
-                                <span class="invisible md:visible">Moje konto </span>
-                                <span class="flex justify-center  pr-5 fa-solid fa-user md:text-sm text-2xl"></span></div>        
+                                    <span class="invisible md:visible">Moje konto </span>
+                                    <span class="flex justify-center pr-5 fa-solid fa-user md:text-sm text-2xl"></span>
+                                </div>        
                                 <ul class="absolute hidden text-gray-700 pt-1 group-hover:block">
-                            <li class="">
-                                <a
-                                class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                                href="{{route('my-account')}}"
-                                >Mój profil</a>
-                            </li>
-                            <li class="">
-                            <a class="class=rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Wyloguj') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                            </li>
-                            </ul>
-                        </div>    
+                                    <li>
+                                        <a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="{{ route('my-profile') }}">Mój profil</a>
+                                    </li>
+                                    <li>
+                                        <a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                            {{ __('Wyloguj') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </li>
+                                </ul>
+                            </div>    
                         @else
-                        
-                        <div class=" w-auto md:hidden bg-inherit text-white">
-                            <div class=" hover:text-slate-400">
-                                <span class="invisible md:flex">Moje konto </span>
-                                <span class="flex justify-center items-center  md:pr-5 fa-solid fa-right-to-bracket md:text-sm text-2xl"></span>
-                            </div>        
-                                <ul class=" absolute hidden text-gray-700 pt-5 md:pt-20  md:mr-20 group-hover:block">
-                            <li class="">
-                                <a
-                                class="rounded-t bg-gray-200 hover:bg-gray-400  block whitespace-no-wrap"
-                                href="{{route('login')}}"
-                                >Zaloguj</a>
-                            </li>
-                            <li class="">
-                            <a class="class=rounded-t bg-gray-200 hover:bg-gray-400 py-2 md:px-4 block whitespace-no-wrap" href="{{ route('register') }}">  
-                                Zarejestruj się</a>
-                            </li>
-                            </ul>
-                        </div>    
+                            <div class="w-auto md:hidden bg-inherit text-white">
+                                <div class="hover:text-slate-400">
+                                    <span class="invisible md:flex">Moje konto </span>
+                                    <span class="flex justify-center items-center md:pr-5 fa-solid fa-right-to-bracket md:text-sm text-2xl"></span>
+                                </div>        
+                                <ul class="absolute hidden text-gray-700 pt-5 md:pt-20 md:mr-20 group-hover:block">
+                                    <li>
+                                        <a class="rounded-t bg-gray-200 hover:bg-gray-400 block whitespace-no-wrap" href="{{ route('login') }}">Zaloguj</a>
+                                    </li>
+                                    <li>
+                                        <a class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 md:px-4 block whitespace-no-wrap" href="{{ route('register') }}">Zarejestruj się</a>
+                                    </li>
+                                </ul>
+                            </div>    
 
-                        <div class="hidden md:flex w-auto bg-inherit text-white">
-                        <a href="{{ route('login') }}"class=" hover:text-blue-200 pr-5 text-xl">Logowanie</a>
-                        </div>
-                            @if (Route::has('register'))
-                            <div class="hidden md:flex md:w-auto md:bg-inherit md:text-white">
-                            <a href="{{ route('register') }}"class=" hover:text-blue-200 pr-5 text-xl">Rejestracja</a>
+                            <div class="hidden md:flex w-auto bg-inherit text-white">
+                                <a href="{{ route('login') }}" class="hover:text-blue-200 pr-5 text-xl">Logowanie</a>
                             </div>
+                            @if (Route::has('register'))
+                                <div class="hidden md:flex md:w-auto md:bg-inherit md:text-white">
+                                    <a href="{{ route('register') }}" class="hover:text-blue-200 pr-5 text-xl">Rejestracja</a>
+                                </div>
                             @endif
                         @endauth
-                        @endif     
-                    </button>
-                </div>
+                    @endif     
+                </button>
             </div>
         </div>
-
-        <!-- <div id="sideNavContainer" style="display: none;">
-
-        </div> -->
-
-        <div class="w-screen ">
-            <form action="search.php" method="GET" class="search-form">
-                <div class="flex border-2 border-white">
-                    <input type="search" name="search" placeholder="Wyszukaj..." class="w-screen border-none focus:outline-none focus:ring-0">                   
-                    <button type="submit" class="w-auto pr-5 bg-white"><i class="fa fa-search bg-white text-xl " ></i></button>
-                </div>
-            </form>
-        </div>
-
-        <div class="mainSectionContainer">
-
-
-        </div>
-
     </div>
 
+        <form action="search.php" method="GET" class="search-form">
+            <div class="flex border-2 border-white">
+                <input type="search" name="search" placeholder="Wyszukaj..." class="w-screen border-none focus:outline-none focus:ring-0">                   
+                <button type="submit" class="pr-5 bg-white">
+                    <i class="fa fa-search bg-white text-xl"></i>
+                </button>
+            </div>
+        </form>
+    <div>
+        <ul class="m-5 p-5 grid gap-4 grid-cols-1 md:grid-cols-4 ">
+            <li class="w-auto">
+                <div class="p-2 bg-white rounded-lg">
+                    <div>
+                        <img class="h-full w-full" src="{{ asset('img/images/icons/car.png') }}" alt="car">
+                    </div>
+                    <div class="flex justify-center p-2"><a class="truncate hover:text-slate-400">Nazwa produktu</a></div>
+                    <div class="text-sm">Miejsce i data</div>
+                    <div class="p2 text-lg">cena</div>
+                </div>
+            </li>
+            <li class="w-auto md:w-30">
+                <div class="p-2 bg-white rounded-lg">
+                    <div>
+                        <img class="h-full w-full" src="{{ asset('img/images/icons/car.png') }}" alt="car">
+                    </div>
+                    <div class="flex justify-center p-2"><a class="truncate hover:text-slate-400">Nazwa produktu</a></div>
+                    <div class="text-sm">Miejsce i data</div>
+                    <div class="p2 text-lg">cena</div>
+                </div>
+            </li>
+            <li class="w-auto md:w-30">
+                <div class="p-2 bg-white rounded-lg">
+                    <div>
+                        <img class="h-full w-full" src="{{ asset('img/images/icons/car.png') }}" alt="car">
+                    </div>
+                    <div class="flex justify-center p-2"><a class="truncate hover:text-slate-400">Nazwa produktu</a></div>
+                    <div class="text-sm">Miejsce i data</div>
+                    <div class="p2 text-lg">cena</div>
+                </div>
+            </li>
+            <li class="w-auto md:w-30">
+                <div class="p-2 bg-white rounded-lg">
+                    <div>
+                        <img class="h-full w-full" src="{{ asset('img/images/icons/car.png') }}" alt="car">
+                    </div>
+                    <div class="flex justify-center p-2"><a class="truncate hover:text-slate-400">Nazwa produktu</a></div>
+                    <div class="text-sm">Miejsce i data</div>
+                    <div class="p2 text-lg">cena</div>
+                </div>
+            </li>  
+        </ul>  
+    </div>
+    
 </body>
-
 </html>
