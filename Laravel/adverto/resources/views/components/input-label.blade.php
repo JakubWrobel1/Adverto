3a3701e1b668ec2f9a38eb22e48cf912fc5e8a3f
JakubWrobel1 <kubawbl@gmail.com>
2023-06-08 18:33:06 +0200

    update

--- Laravel/adverto/resources/views/components/input-label.blade.php
+++ Laravel/adverto/resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
+<label {{ $attributes->merge(['class' => 'w-full rounded-sm border-none bg-[#f2f4f5] shadow-sm mb-2']) }}>
     {{ $value ?? $slot }}
 </label>
